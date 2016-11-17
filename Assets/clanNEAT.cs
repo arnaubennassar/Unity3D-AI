8d10a3f7ae74d01f59f4dd81fbfc0c45b9db520f
arnau bennassar <arnaubennassar5@gmail.com>
2016-11-17 12:16:20 +0100

    NEAT added (unityNEAT:  https://github.com/lordjesus/UnityNEAT  )

---
+++ Assets/clanNEAT.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using SharpNeat.Phenomes;
+
+public class clanNEAT : UnitController {
+	bool IsRunning;
+	IBlackBox box;
+
+	void FixedUpdate() {
+			
+		}
+
+	public override void Stop()
+	{
+		this.IsRunning = false;
+	}
+
+	public override void Activate(IBlackBox box)
+	{
+		this.box = box;
+		this.IsRunning = true;
+	}
+
+	public override float GetFitness()
+	{
+		// Implement a meaningful fitness function here, for each unit.
+		return 0;
+	}
+}
